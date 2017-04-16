2992820c5277d59854113a9f7d4324c3335ed5b2
Danny Maher <dmaher724@gmail.com>
2017-04-16 13:02:11 -0500

    Write iterative method to calculate nth Fibonacci number

--- the-fibonacci-sequence-challenge-master/fibonacci.rb
+++ the-fibonacci-sequence-challenge-master/fibonacci.rb
@@ -1,4 +1,16 @@
+# Note: In this challenge the Fibonacci sequence is zero-indexed and the first term is 0
+
 def iterative_nth_fibonacci_number(n)
+  return 0 if n == 0
+  return 1 if n == 1
+
+  fibs = [0, 1]
+
+  until fibs.length == n + 1
+    fibs << fibs[-1] + fibs[-2]
+  end
+
+  fibs[n]
 end
 
 def recursive_nth_fibonacci_number(n)
