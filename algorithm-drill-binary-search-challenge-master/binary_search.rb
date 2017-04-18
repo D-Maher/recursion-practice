8899d3fb654192dec5c3a17a5dd6139817b21c8a
Danny Maher <dmaher724@gmail.com>
2017-04-18 17:29:06 -0500

    Add benchmarking to compare performances of binary search, linear search, and Ruby's built-in Array#index

--- algorithm-drill-binary-search-challenge-master/binary_search.rb
+++ algorithm-drill-binary-search-challenge-master/binary_search.rb
@@ -23,3 +23,46 @@ def binary_search(array, target, midpoint_index = array.length / 2)
   end
 
 end
+
+def linear_search(array, target)
+  idx = 0
+
+  while idx < array.length
+    if array[idx] == target
+      return idx
+    else
+      idx += 1
+    end
+  end
+
+  nil
+end
+
+
+require 'benchmark'
+
+Benchmark.bm do |x|
+
+  test_array = ("a".."eeeee").to_a
+  target = "dbc"
+
+  x.report("binary search:") { binary_search(test_array, target) }
+
+  puts "-" * 70
+
+  x.report("linear search:") { linear_search(test_array, target) }
+
+  puts "-" * 70
+
+  x.report("Array#index:") { test_array.index(target) }
+
+end
+
+# Benchmark output:
+
+#                   user     system      total        real
+# binary search:  0.000000   0.000000   0.000000 (  0.007298)
+# ----------------------------------------------------------------------
+# linear search:  0.000000   0.000000   0.000000 (  0.000901)
+# ----------------------------------------------------------------------
+# Array#index:    0.000000   0.000000   0.000000 (  0.000109)
