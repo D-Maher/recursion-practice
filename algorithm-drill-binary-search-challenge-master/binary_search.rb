a92070fc9b81f4031f5dac2a8c00a221b551ce8f
Danny Maher <dmaher724@gmail.com>
2017-04-18 16:30:19 -0500

    Add additional tests, clean up code

--- algorithm-drill-binary-search-challenge-master/binary_search.rb
+++ algorithm-drill-binary-search-challenge-master/binary_search.rb
@@ -1,22 +1,14 @@
 def binary_search(array, target, midpoint_index = array.length / 2)
-  return nil if array.length == 1 && array[0] != target
+  return nil if array.length <= 1 && array[0] != target
 
   left = array[0...midpoint_index]
   right = array[midpoint_index..-1]
 
-  p "array: #{array}"
-  p "midpoint_index: #{midpoint_index}"
-
-  p "left: #{left}"
-  p "right: #{right}"
-
   if array[midpoint_index] == target
     midpoint_index
   elsif array[midpoint_index] > target
-    p "SEARCHING LEFT"
     binary_search(left, target)
   elsif array[midpoint_index] < target
-    p "SEARCHING RIGHT"
     if binary_search(right, target) == nil
       return nil
     else
--- algorithm-drill-binary-search-challenge-master/spec/binary_search_spec.rb
+++ algorithm-drill-binary-search-challenge-master/spec/binary_search_spec.rb
@@ -2,7 +2,19 @@ require_relative '../binary_search'
 
 describe 'recursive binary_search' do
 
-    it 'will return the index of a target element in an array if the element exists in the array' do
+    it 'will return the index of a target in the exact middle of an array' do
+      array = [:a, :b, :c, :d, :e, :f, :g, :h, :j]
+
+      expect(binary_search(array, :e)).to eq 4
+    end
+
+    it 'will return the index of a target element on the left side of an array' do
+      array = [:a, :b, :c, :d, :e, :f, :g, :h]
+
+      expect(binary_search(array, :c)).to eq 2
+    end
+    
+    it 'will return the index of a target on the right side of an array' do
       array = [:a, :b, :c, :d, :e, :f, :g, :h]
 
       expect(binary_search(array, :g)).to eq 6
@@ -12,6 +24,12 @@ describe 'recursive binary_search' do
       array = [:a, :b, :c, :d, :e, :f, :g, :h]
 
       expect(binary_search(array, :x)).to eq nil
+    end
+
+    it 'will return nil if the array is empty' do
+      array = []
+
+      expect(binary_search(array, :e)).to eq nil
     end
   
 end
