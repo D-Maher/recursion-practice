2f12004a944e6bdf457399f6ee52a78ae5283428
Danny Maher <dmaher724@gmail.com>
2017-04-18 09:59:37 -0500

    Write basic tests for binary search method

--- algorithm-drill-binary-search-challenge-master/spec/binary_search_spec.rb
+++ algorithm-drill-binary-search-challenge-master/spec/binary_search_spec.rb
@@ -1,4 +1,17 @@
 require_relative '../binary_search'
 
-describe 'binary_search' do
+describe 'recursive binary_search' do
+
+    it 'will return the index of a target element in an array if the element exists in the array' do
+      array = [:a, :b, :c, :d, :e, :f, :g, :h]
+
+      expect(binary_search(array, :g)).to eq 6
+    end
+
+    it 'will return nil if the element does not exist in the array' do
+      array = [:a, :b, :c, :d, :e, :f, :g, :h]
+
+      expect(binary_search(array, :x)).to eq nil
+    end
+  
 end
